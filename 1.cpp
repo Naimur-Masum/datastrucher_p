13d54e3fb1bee7558a23281c340de7e5faeff216
Md.Naimur Rahman Masum <113965701+Naimur-Masum@users.noreply.github.com>
2022-09-27 13:43:44 +0600

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int arr_1[]={10,20,30,40,50};
+   int  arr_2[]={1,2,3,4,5,6,7,8};
+   int arr[100];
+
+
+        for(int i=7;i>=0;i--){
+        cout<< arr_2[i]<<" ";
+        }
+
+        for(int i=4;i>=0;i--){
+        cout<<arr_1[i]<<" ";
+        }
+
+    return 0;
+}
+
